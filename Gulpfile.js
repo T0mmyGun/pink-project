b0c52d007b6a330567430e57ccb3170efa0684b1
T0mmyGun <gur0guro95@gmail.com>
2018-06-14 14:27:04 +0300

    Добавлена автоматизация, в процессе мобильная и десктоп версии

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,6 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
-var plumber = require('gulp-plumber');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
 
 
 gulp.task('style', function(){
